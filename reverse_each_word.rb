09e716a82685361b3a12bf37321e6ff69af041df
Alec German <alec_german@yahoo.com>
2018-02-02 18:02:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
 def reverse_each_word(sentence)
-  sentence.split(' ').map{|word| word.split('').reverse.join('')}.join(' ')
+  sentence.split(' ').collect{|word| word.split('').reverse.join('')}.join(' ')
 end
